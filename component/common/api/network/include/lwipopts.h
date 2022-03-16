e0c0f430d5f5f17f7d9315da683782e8542131c3
step0035 <step0035@e.ntu.edu.sg>
2022-03-16 18:47:24 +0800

    Increase buffer size for data that are 1282 in size

--- component/common/api/network/include/lwipopts.h
+++ component/common/api/network/include/lwipopts.h
@@ -111,7 +111,7 @@ a lot of data that needs to be copied, this should be set high. */
 #endif
 
 /* PBUF_POOL_BUFSIZE: the size of each pbuf in the pbuf pool. */
-#define PBUF_POOL_BUFSIZE       1280
+#define PBUF_POOL_BUFSIZE       1300
 
 
 /* ---------- TCP options ---------- */
