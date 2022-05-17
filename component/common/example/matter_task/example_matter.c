68b104a5cb6c7b3e5f64d1e2f7b698546f70e07c
xushuqun <xushuqun@realtek-sg.com>
2022-05-17 11:06:34 +0800

    [WiFi] add conditions to wait for auto-reconnection
    * Wait for auto-reconnection to complete before starting ChipTest() after reboot
    * This is to prevent BLEManager initialization failed and DeviceManagerInit() error as init_thread fails to finish and driver attempts to retry connection
    * This also helps to fix cannot allocate pbuf if auto-reconnection is performed

--- component/common/example/matter_task/example_matter.c
+++ component/common/example/matter_task/example_matter.c
@@ -4,6 +4,11 @@
 #include "basic_types.h"
 #include "platform_opts.h"
 #include "section_config.h"
+#if CONFIG_EXAMPLE_WLAN_FAST_CONNECT && CONFIG_FAST_DHCP
+#include "wifi/wifi_conf.h"
+#include "wlan_fast_connect/example_wlan_fast_connect.h"
+extern uint32_t offer_ip;
+#endif
 
 #if defined(CONFIG_EXAMPLE_MATTER) && CONFIG_EXAMPLE_MATTER
 
@@ -12,16 +17,30 @@ extern void ChipTest(void);
 static void example_matter_task_thread(void *pvParameters)
 {
 	vTaskDelay(5000);
+
+#if CONFIG_EXAMPLE_WLAN_FAST_CONNECT && CONFIG_FAST_DHCP
+	int autoreconnect_count = 0;
+
+	/*Wait for internet connection if fast connect has data*/
+	while((wifi_is_ready_to_transceive(RTW_STA_INTERFACE) != RTW_SUCCESS) && (offer_ip)){
+		printf("\n\r[WiFi] exist network: wait for Wi-Fi connection");
+		autoreconnect_count++;
+		vTaskDelay(AUTO_RECONNECT_INTERVAL*1000);
+		if(autoreconnect_count > (AUTO_RECONNECT_COUNT + 2))
+			break;
+	}
+#endif
+
 	ChipTest();
 
-    vTaskDelete(NULL);
-    return;
+	vTaskDelete(NULL);
+	return;
 }
 
 void example_matter_task(void)
 {
-    if(xTaskCreate(example_matter_task_thread, ((const char*)"example_matter_task_thread"), 1024, NULL, tskIDLE_PRIORITY + 1, NULL) != pdPASS)
-        printf("\n\r%s xTaskCreate(example_matter_task_thread) failed", __FUNCTION__);
+	if(xTaskCreate(example_matter_task_thread, ((const char*)"example_matter_task_thread"), 1024, NULL, tskIDLE_PRIORITY + 1, NULL) != pdPASS)
+		printf("\n\r%s xTaskCreate(example_matter_task_thread) failed", __FUNCTION__);
 }
 
 #endif // #if (CONFIG_EXAMPLE_MATTER)
